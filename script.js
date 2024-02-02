71a02d850d0c7ca3252c4ab4ec3a1b3f52bd1640
SonuKumar123098 <112372023+SonuKumar123098@users.noreply.github.com>
2024-02-02 13:30:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const makeChange = (c) => {
   // your name here
+	res={};
+	c=Number(c);
+	res.q=c/25; c%=25;
+	res.d=c/10; c%=10;
+	res.n=c/5; c%=5;
+	res.p=c/1;
+	
 };
 
 // Do not the change the code below
